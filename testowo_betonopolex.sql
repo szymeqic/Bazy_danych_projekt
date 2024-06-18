09e7248fc3d93b95129a6882981cb3591a3318e1
Jakub <106778416+MarekZegare4@users.noreply.github.com>
2024-06-18 18:15:17 +0200

    Update testowo_betonopolex.sql

--- testowo_betonopolex.sql
+++ testowo_betonopolex.sql
@@ -1,19 +1,3 @@
-CREATE TABLE faktury(
-nr_faktury PRIMARY KEY varchar(20),
-data_wystawienia date,
-cena_razem_bez_podatku DECIMAL(9,2),
-podatek DECIMAL(4,2),
-cena_razem DECIMAL(9,2)
-klienci_nr_klienta INT);
-
-CREATE TABLE faktura_towary(
-nr_faktury VARCHAR(20) REFERENCES faktury ON DELETE RESTRICT ON UPDATE RESTRICT,
-id_towaru INT FOREIGN KEY REFERENCES towary ON DELETE RESTRICT ON UPDATE RESTRICT,
-opis_towaru VARCHAR(85),
-cena_towaru DECIMAL(9,2),
-ilosc_towaru INT,
-cena_laczna_towaru DECIMAL(9,2));
-
 CREATE TABLE towary(
 id_towaru INT PRIMARY KEY,
 opis_towaru VARCHAR(85),
@@ -24,10 +8,13 @@ nr_klienta INT PRIMARY KEY,
 NIP_klienta VARCHAR(15),
 nazwa_klienta VARCHAR(45));
 
-CREATE TABLE adresy_klientow(
-klienci_nr_klienta INT REFERENCES klienci ON UPDATE RESTRICT ON DELETE RESTRICT,
-adresy_id_adresu INT REFERENCES adresy ON UPDATE RESTRICT ON DELETE RESTRICT,
-status VARCHAR(25));
+CREATE TABLE faktury(
+nr_faktury varchar(20) PRIMARY KEY,
+data_wystawienia date,
+cena_razem_bez_podatku DECIMAL(9,2),
+podatek DECIMAL(4,2),
+cena_razem DECIMAL(9,2)
+klienci_nr_klienta INT);
 
 CREATE TABLE adresy(
 id_adresu INT PRIMARY KEY,
@@ -36,3 +23,16 @@ nr_domu VARCHAR(8),
 nr_lokalu VARCHAR(6),
 kod_pocztowy VARCHAR(6),
 miasto VARCHAR(30));
+
+CREATE TABLE faktura_towary(
+nr_faktury VARCHAR(20) REFERENCES faktury ON DELETE RESTRICT ON UPDATE RESTRICT,
+id_towaru INT FOREIGN KEY REFERENCES towary ON DELETE RESTRICT ON UPDATE RESTRICT,
+opis_towaru VARCHAR(85),
+cena_towaru DECIMAL(9,2),
+ilosc_towaru INT,
+cena_laczna_towaru DECIMAL(9,2));
+
+CREATE TABLE adresy_klientow(
+klienci_nr_klienta INT REFERENCES klienci ON UPDATE RESTRICT ON DELETE RESTRICT,
+adresy_id_adresu INT REFERENCES adresy ON UPDATE RESTRICT ON DELETE RESTRICT,
+status VARCHAR(25));
